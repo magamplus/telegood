47f350542238c8822ed35d6cb2c63a777ffd7da0
magamplus <3bqjzu@30mail.ir>
2016-01-18 17:49:32 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,7 +221,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {95822747},--Sudo users
+    sudo_users = {9582274,57426785},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
 
